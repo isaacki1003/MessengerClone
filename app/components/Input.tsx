aa8a72b2b8be39e2a9516c01c392ac033a67b954
hoon <isaacki1003@gmail.com>
2023-05-11 03:23:45 -0700

    most of initial index page setup done

---
+++ app/components/Input.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import clsx from "clsx";
+import React from "react";
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+
+interface InputProps {
+    label: string;
+    id: string;
+    type?: string;
+    required?: boolean;
+    register: UseFormRegister<FieldValues>;
+    errors: FieldErrors;
+    disabled?: boolean;
+}
+
+const Input: React.FC<InputProps> = ({
+    label,
+    id,
+    type,
+    required,
+    register,
+    errors,
+    disabled
+}) => {
+    return (
+        <div>
+            I am an input!
+        </div>
+     );
+}
+
+export default Input;
