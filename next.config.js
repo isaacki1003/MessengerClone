3d29a89732b1ce9ae710018eb1475fd6a5efd220
hoon <isaacki1003@gmail.com>
2023-05-30 16:40:38 -0700

    need to work on messages not sending

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        appDir: true,
-        swcPlugins: [
-            ["next-superjson-plugin", {}]
-        ]
+      appDir: true,
+      swcPlugins: [["next-superjson-plugin", {}]]
     },
-}
-
-module.exports = nextConfig
+    images: {
+      domains: [
+        'res.cloudinary.com', 
+        'avatars.githubusercontent.com',
+        'lh3.googleusercontent.com'
+      ]
+    }
+  }
+  
+  module.exports = nextConfig
